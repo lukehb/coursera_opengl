ce09961a361a278884e024dd5c5e7280d07d9a6e
luke bermingham <luke.bermingham@my.jcu.edu.au>
2015-07-12 19:46:30 +1000

    directory thing still not working, trying some SO code

--- index.php
+++ index.php
@@ -1,74 +1,29 @@
 <?php
-$pathLen = 0;
+$pngFolder = <<< EOFILE
+iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAA3NCSVQICAjb4U/gAAABhlBMVEX//v7//v3///7//fr//fj+/v3//fb+/fT+/Pf//PX+/Pb+/PP+/PL+/PH+/PD+++/+++7++u/9+vL9+vH79+r79+n79uj89tj89Nf889D88sj78sz78sr58N3u7u7u7ev777j67bL67Kv46sHt6uP26cns6d356aP56aD56Jv45pT45pP45ZD45I324av344r344T14J734oT34YD13pD24Hv03af13pP233X025303JL23nX23nHz2pX23Gvn2a7122fz2I3122T12mLz14Xv1JPy1YD12Vz02Fvy1H7v04T011Py03j011b01k7v0n/x0nHz1Ejv0Hnuz3Xx0Gvz00buzofz00Pxz2juz3Hy0TrmznzmzoHy0Djqy2vtymnxzS3xzi/kyG3jyG7wyyXkwJjpwHLiw2Liw2HhwmDdvlXevVPduVThsX7btDrbsj/gq3DbsDzbrT7brDvaqzjapjrbpTraojnboTrbmzrbmjrbl0Tbljrakz3ajzzZjTfZijLZiTJdVmhqAAAAgnRSTlP///////////////////////////////////////8A////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////9XzUpQAAAAlwSFlzAAALEgAACxIB0t1+/AAAAB90RVh0U29mdHdhcmUATWFjcm9tZWRpYSBGaXJld29ya3MgOLVo0ngAAACqSURBVBiVY5BDAwxECGRlpgNBtpoKCMjLM8jnsYKASFJycnJ0tD1QRT6HromhHj8YMOcABYqEzc3d4uO9vIKCIkULgQIlYq5haao8YMBUDBQoZWIBAnFtAwsHD4kyoEA5l5SCkqa+qZ27X7hkBVCgUkhRXcvI2sk3MCpRugooUCOooWNs4+wdGpuQIlMDFKiWNbO0dXTx9AwICVGuBQqkFtQ1wEB9LhGeAwDSdzMEmZfC0wAAAABJRU5ErkJggg==
+EOFILE;
 
-function prePad($level)
+if (isset($_GET['img']))
 {
-  $ss = "";
-
-  for ($ii = 0;  $ii < $level;  $ii++)
-  {
-    $ss = $ss . "|&nbsp;&nbsp;";
-  }
-
-  return $ss;
+       header("Content-type: image/png");
+        echo base64_decode($pngFolder);
+        exit();
 }
 
-function myScanDir($dir, $level, $rootLen)
+
+$projectsListIgnore = array ('.','..'); 
+$handle=opendir(".");
+$projectContents = '';
+while ($file = readdir($handle)) 
 {
-  global $pathLen;
-
-  if ($handle = opendir($dir)) {
-
-    $allFiles = array();
-
-    while (false !== ($entry = readdir($handle))) {
-      if ($entry != "." && $entry != "..") {
-        if (is_dir($dir . "/" . $entry))
-        {
-          $allFiles[] = "D: " . $dir . "/" . $entry;
-        }
-        else
-        {
-          $allFiles[] = "F: " . $dir . "/" . $entry;
-        }
-      }
+    if (is_dir($file) && !in_array($file,$projectsListIgnore)) 
+    {       
+        $projectContents .= '<li><a href="'.$file.'">'.$file.'</a></li>';
     }
-    closedir($handle);
-
-    natsort($allFiles);
-
-    foreach($allFiles as $value)
-    {
-      $displayName = substr($value, $rootLen + 4);
-      $fileName    = substr($value, 3);
-      $linkName    = str_replace(" ", "%20", substr($value, $pathLen + 3));
-      if (is_dir($fileName)) {
-        echo prePad($level) . $linkName . "<br>\n";
-        myScanDir($fileName, $level + 1, strlen($fileName));
-      } else {
-        echo prePad($level) . "<a href=\"" . $linkName . "\" style=\"text-decoration:none;\">" . $displayName . "</a><br>\n";
-      }
-    }
-  }
 }
+closedir($handle);
 
-?><!DOCTYPE HTML>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Site Map</title>
-</head>
-
-<body>
-<h1>Site Map</h1>
-<p style="font-family:'Courier New', Courier, monospace; font-size:small;">
-<?php
-  $root = '/code';
-
-  $pathLen = strlen($root);
-
-  myScanDir($root, 0, strlen($root)); ?>
-</p>
-</body>
-
-</html>
+?>
+<ul class="projects">
+<?php $projectContents ?>
+</ul>
