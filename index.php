29df5ccfb0991a556dff823d39d202beaf60edb6
luke bermingham <luke.bermingham@my.jcu.edu.au>
2015-07-12 19:38:44 +1000

    changing dir lister again

--- index.php
+++ index.php
@@ -1,12 +1,74 @@
 <?php
+$pathLen = 0;
 
-<?php
-$files = scandir('./code');
-sort($files); // this does the sorting
-foreach($files as $file){
-   echo'<a href="/code/'.$file.'">'.$file.'</a>';
-   echo'</br>';
+function prePad($level)
+{
+  $ss = "";
+
+  for ($ii = 0;  $ii < $level;  $ii++)
+  {
+    $ss = $ss . "|&nbsp;&nbsp;";
+  }
+
+  return $ss;
 }
-?>
 
-?>
+function myScanDir($dir, $level, $rootLen)
+{
+  global $pathLen;
+
+  if ($handle = opendir($dir)) {
+
+    $allFiles = array();
+
+    while (false !== ($entry = readdir($handle))) {
+      if ($entry != "." && $entry != "..") {
+        if (is_dir($dir . "/" . $entry))
+        {
+          $allFiles[] = "D: " . $dir . "/" . $entry;
+        }
+        else
+        {
+          $allFiles[] = "F: " . $dir . "/" . $entry;
+        }
+      }
+    }
+    closedir($handle);
+
+    natsort($allFiles);
+
+    foreach($allFiles as $value)
+    {
+      $displayName = substr($value, $rootLen + 4);
+      $fileName    = substr($value, 3);
+      $linkName    = str_replace(" ", "%20", substr($value, $pathLen + 3));
+      if (is_dir($fileName)) {
+        echo prePad($level) . $linkName . "<br>\n";
+        myScanDir($fileName, $level + 1, strlen($fileName));
+      } else {
+        echo prePad($level) . "<a href=\"" . $linkName . "\" style=\"text-decoration:none;\">" . $displayName . "</a><br>\n";
+      }
+    }
+  }
+}
+
+?><!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Site Map</title>
+</head>
+
+<body>
+<h1>Site Map</h1>
+<p style="font-family:'Courier New', Courier, monospace; font-size:small;">
+<?php
+  $root = '/code';
+
+  $pathLen = strlen($root);
+
+  myScanDir($root, 0, strlen($root)); ?>
+</p>
+</body>
+
+</html>
